3a0113fd3cff4a2ef1654bbfd4393eb72ea3089c
parthsingh <parth119516@gmail.com>
2024-03-21 19:51:04 -0700

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-// App.js or your route configuration file
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom'; // Change HashRouter to BrowserRouter
 import Home from './pages/home';
 import About from './pages/about';
 import Projects from './pages/projects';
 import Courses from './pages/courses';
 import Contact from './pages/contact';
-// ... import other pages
 
 const App = () => {
   return (
-    <Router>
+    <Router basename='/'> {/* Change HashRouter to BrowserRouter */}
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path ="/projects" element={<Projects/>}/>
-        <Route path = "/coursework" element={<Courses/>}/>
-        <Route path = "/contact" element={<Contact/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/coursework" element={<Courses />} />
+        <Route path="/contact" element={<Contact />} />
         {/* ... add routes for other pages */}
       </Routes>
-    </Router>
+    </Router> 
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { HashRouter } from "react-router-dom";
-
+import App from './App';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
-//   <React.StrictMode>
+//   <HashRouter>
 //     <App />
-//   </React.StrictMode>
+//   </HashRouter>
 // );
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 );
-reportWebVitals();
+
